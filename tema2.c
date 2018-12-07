b699fee9763ed7bbd4553505176504b3eca23f0d
alexcareja <44427360+alexcareja@users.noreply.github.com>
2018-12-07 03:32:21 +0200

    Add files via upload

--- tema2.c
+++ tema2.c
@@ -11,6 +11,8 @@ char *split(char *, int, int);
 int char_to_int(char *, int);
 char **prep_wrap(char **, char*, int *);
 char **wrap(char **, int *, int);
+char **prep_center(char **, char *, int *);
+void center(char **, int, int, int);
 char **prep_align_left(char **, char *, int *);
 void align_left(char **, int, int);
 char **prep_align_right(char **, char *, int *);
@@ -154,6 +156,7 @@ char **commands(char **operations,
 	   original = prep_wrap(original, operations[i], original_line_count);
 	   break;
 	case 'c':
+	   original = prep_center(original, operations[i], original_line_count);
 	   break;
 	case 'l':
 	   original = prep_align_left(original, operations[i], original_line_count);
@@ -306,6 +309,80 @@ char **wrap(char **original, int *original_line_count, int max_line_length)
   	   }
   *original_line_count = line;
   return text;
+}
+
+char **prep_center(char **original, char *operations, int *original_line_count){
+  int start_line, end_line, k = 0; 
+  char *start_line_string, *end_line_string = NULL, 
+       *token = (char *) calloc(MAX_CHAR, sizeof(char));
+  token = strtok(operations, " ");
+  while ( token != NULL ){
+     k++;
+     if (k == 2){
+	start_line_string = token;
+     }
+     if (k == 3){
+	end_line_string = token;
+     }
+     if (k > 3){
+	printf("Invalid operation!\n");
+	return NULL;
+     }
+     token = strtok(NULL, " ");
+  }
+  free(token);
+  if (k == 1){
+     start_line = 0;
+     end_line = *original_line_count - 1;
+  }
+  else{
+     start_line = char_to_int(start_line_string, strlen(start_line_string));
+     if (start_line == -1){
+	printf("Invalid operation!\n");
+	return NULL;
+     }
+     if (end_line_string == NULL){
+	end_line = *original_line_count - 1;
+     }
+     else{
+	end_line = char_to_int(end_line_string, strlen(end_line_string));
+	if (end_line == -1){
+	   printf("Invalid operation!\n");
+	   return NULL;
+        }
+	if (end_line < start_line){
+	   printf("Invalid operation!\n");
+           return NULL;
+	}
+	if (end_line >= *original_line_count){
+	   end_line = *original_line_count - 1;
+	}
+     }
+  }
+  int max_length = strlen(original[0]);
+  for (k = 1; k < *original_line_count; k++){
+     if (strlen(original[k]) > max_length){
+	max_length = strlen(original[k]);
+     }
+  }
+  center(original, start_line, end_line, max_length);
+  return original;
+}
+
+void center(char **original, int start_line, int end_line, int max_length){
+  int i, spaces;
+  char *line = (char *) calloc(1000, sizeof(char));
+  for (i = start_line; i <= end_line; i++){
+     spaces = max_length - strlen(original[i]);
+     spaces = (spaces + 1) / 2;
+     while ( spaces ){
+	strcpy(line, original[i]);
+	strcpy(original[i], " ");
+	strcat(original[i], line);
+	spaces--;
+     }
+  }
+  free(line);
 }
 
 char **prep_align_left(char **original, char *operations, int *original_line_count){
